277a4eb1f2bbde306433a72a59ab1c9f6a5fb04b
Harshit Joshi <harshitjoshi802@gmail.com>
2022-05-03 15:16:37 +0530

    QR CodeScanner

---
+++ app/src/main/java/com/example/codescan/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.codescan
+
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.budiyev.android.codescanner.*
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var codeScanner: CodeScanner
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val scannerView = findViewById<CodeScannerView>(R.id.mainView)
+
+        codeScanner = CodeScanner(this, scannerView)
+
+
+        codeScanner.camera = CodeScanner.CAMERA_BACK
+        codeScanner.formats = CodeScanner.ALL_FORMATS
+        codeScanner.autoFocusMode = AutoFocusMode.SAFE
+        codeScanner.scanMode = ScanMode.SINGLE
+        codeScanner.isAutoFocusEnabled = true
+        codeScanner.isFlashEnabled = false
+
+
+        codeScanner.decodeCallback = DecodeCallback {
+            runOnUiThread {
+                val browserIntent =
+                    Intent(Intent.ACTION_VIEW, Uri.parse("${it.text}"))
+                startActivity(browserIntent)
+            }
+        }
+        codeScanner.errorCallback = ErrorCallback {
+            runOnUiThread {
+                Toast.makeText(this, "Camera initialization error: ${it.message}", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        scannerView.setOnClickListener {
+            codeScanner.startPreview()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+}
